b7553a11e397cebc625986cd3c760bc487db4cdf
Ahmed Gado <ahmedgado2001@gmail.com>
2020-04-12 15:28:35 +0200

    Initial Commit

---
+++ DetailsStrategy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetailsStrategy
+//
+//  Created by ahmed gado on 4/12/20.
+//  Copyright © 2020 ahmed gado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
